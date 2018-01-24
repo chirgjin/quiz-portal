f1b562238a43dec4de43a41cbef2fb027cb594a0
shreyansdjp <shreyansjain68@gmail.com>
2018-01-24 13:10:41 +0530

    session thoda sa implement kr dia GG

--- backend/login.php
+++ backend/login.php
@@ -1,58 +1,76 @@
 <?php
 require_once __DIR__ . "/user.class.php";
 
-// class Verification
-// {
-//     // simple function to achieve connection to DB with pdo..
-//     private function connectToDB() {
-//         require  __DIR__ . '/dbconfig.php';
-//         return $pdo;
-//     }
-//
-//     private function vTeam($pdo, $teamName, $teamCode) {
-//         $sql = "SELECT * FROM users WHERE team_name = ? AND team_code = ?";
-//         $stmt = $pdo->prepare($sql);
-//         $stmt->execute([$teamName, $teamCode]);
-//         if($stmt->rowCount() > 0) {
-//             return $stmt->fetchAll();
-//         } else {
-//             return false;
-//         }
-//     }
-//
-//     public function verifyTeam($teamName, $teamCode) {
-//         $pdo = $this->connectToDB();
-//         return $this->vTeam($pdo, $teamName, $teamCode);
-//     }
-//
-//     private function vLone($pdo, $email, $phone) {
-//         $sql = 'SELECT * FROM users WHERE email = :email AND phone = :phone';
-//         $stmt = $pdo->prepare($sql);
-//         $stmt->execute(['email' => $email, 'phone' => $phone]);
-//         if($stmt->rowCount() > 0) {
-//             return $stmt->fetchAll();
-//         } else {
-//             return false;
-//         }
-//     }
-//
-//     public function verifyLoneWolf($email, $phone) {
-//         $pdo = $this->connectToDB();
-//         return $this->vLone($pdo, $email, $phone);
-//     }
-//
-// }
+/*
+class Verification
+{
+    // simple function to achieve connection to DB with pdo..
+    private function connectToDB() {
+        require  __DIR__ . '/dbconfig.php';
+        return $pdo;
+    }
+
+    private function vTeam($pdo, $teamName, $teamCode) {
+        $sql = "SELECT * FROM users WHERE team_name = ? AND team_code = ?";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$teamName, $teamCode]);
+        if($stmt->rowCount() > 0) {
+            return $stmt->fetchAll();
+        } else {
+            return false;
+        }
+    }
+
+    public function verifyTeam($teamName, $teamCode) {
+        $pdo = $this->connectToDB();
+        return $this->vTeam($pdo, $teamName, $teamCode);
+    }
+
+    private function vLone($pdo, $email, $phone) {
+        $sql = 'SELECT * FROM users WHERE email = :email AND phone = :phone';
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['email' => $email, 'phone' => $phone]);
+        if($stmt->rowCount() > 0) {
+            return $stmt->fetchAll();
+        } else {
+            return false;
+        }
+    }
+
+    public function verifyLoneWolf($email, $phone) {
+        $pdo = $this->connectToDB();
+        return $this->vLone($pdo, $email, $phone);
+    }
+
+}
+*/
 
 $verify = new USER;
+
+if(!isset($_POST)) {
+    echo 'post not set';
+}
 
 if($_POST['isTeam'] == 'false') {
     $verify->email = $_POST['email'];
     $verify->phone = $_POST['phone'];
     if(!($verify->fetch() == null)) {
+        // session start
+        $_SESSION['id'] = $verify->id;
         echo json_encode(array('success' => true));
+    } else {
+        echo json_encode(array('success' => false));
     }
 } else if($_POST['isTeam'] == 'true') {
-
+    $verify->team_name = $_POST['teamName'];
+    $verify->team_code = $_POST['teamCode'];
+    if(!($verify->fetch() == null)) {
+        // session start
+        $_SESSION['id'] = $verify->id;
+        echo json_encode(array('success' => true));
+    } else {
+        echo json_encode(array('success' => false));
+    }
 }
 
 
--- backend/session.php
+++ backend/session.php
@@ -1,3 +1,15 @@
 <?php
+    require __DIR__ . '/user.class.php';
+    require_once __DIR__ . '/dbconfig.php';
+
+    $user = new USER;
+
+    $user->id = $_SESSION['id'];
+
+    print_r($user->fetch());
+
+    if(isset($_SESSION)) {
+        session_destroy();
+    }
 
 ?>
