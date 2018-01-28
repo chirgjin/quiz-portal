ade944c21ebdc39de3b43173ee10fa2cec72c63a
shreyansdjp <shreyansjain68@gmail.com>
2018-01-28 19:37:55 +0530

    questions made with php file no time for json basically

--- frontend/admin-panel/js/questions.js
+++ frontend/admin-panel/js/questions.js
@@ -4,43 +4,27 @@ const formHTML = function(no_of_questions) {
     return `
         <div class="form-group">
             <label>Question 1</label>
-            <textarea class="form-control" name="question" rows="5"></textarea>
+            <textarea class="form-control" name="question" rows="5" required></textarea>
         </div>
         <div class="form-group">
             <label>Option 1</label>
-            <input type="text" name="option1" class="form-control">
+            <input type="text" name="option1" class="form-control" required>
         </div>
         <div class="form-group">
             <label>Option 2</label>
-            <input type="text" name="option2" class="form-control">
+            <input type="text" name="option2" class="form-control" required>
         </div>
         <div class="form-group">
             <label>Option 3</label>
-            <input type="text" name="option3" class="form-control">
+            <input type="text" name="option3" class="form-control" required>
         </div>
         <div class="form-group">
             <label>Option 4</label>
-            <input type="text" name="option4" class="form-control">
+            <input type="text" name="option4" class="form-control" required>
         </div>
         <div class="form-group">
             <label>Correct Option</label>
-            <br>
-            <label class="radio-inline">
-                <input type="radio" name="correctOption">
-                Option 1
-            </label>
-            <label class="radio-inline">
-                <input type="radio" name="correctOption">
-                Option 2
-            </label>
-            <label class="radio-inline">
-                <input type="radio" name="correctOption">
-                Option 3
-            </label>
-            <label class="radio-inline">
-                <input type="radio" name="correctOption">
-                Option 4
-            </label>
+            <input type="number" name="correct" class="form-control" required>
         </div>
         <hr id="line">
         `;
@@ -49,6 +33,6 @@ const formHTML = function(no_of_questions) {
 $(document).ready(function() {
     $('#add').on('click', function() {
         no_of_questions += 1;
-        $('form').append(formHTML(no_of_questions));
+        $('#sub').before(formHTML(no_of_questions));
     })
 });
